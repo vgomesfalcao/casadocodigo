f01a57265a9f920091cdaecbe99507d625dedcae
Vinicius <vgomesfalcao@gmail.com>
2020-10-27 14:00:46 -0300

    Aula05 modulo 2

---
+++ src/app/routes/routes.js
@@ -0,0 +1,32 @@
+module.exports = (app) =>{
+    app.get('/', function(req,res){
+        res.send(
+            `
+                 <html>
+                     <head>
+                         <meta charset="utf-8">
+                     </head>
+                     <body>
+                         <h1> Casa do Código </h1>
+                     </body>
+                 </html>
+             `
+        )
+    })
+    
+    app.get('/livros', function(req,res){
+        res.send(
+            `
+                 <html>
+                     <head>
+                         <meta charset="utf-8">
+                     </head>
+                     <body>
+                         <h1> Lista de Livros </h1>
+                     </body>
+                 </html>
+             `
+        )
+    })
+}
+
